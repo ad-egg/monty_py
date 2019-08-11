632a00b41e27a8855a758f58eb961c1a123f1937
Athena <539@holbertonschool.com>
2019-08-11 00:45:26 +0000

    pall method calls pint method

--- print_stack.py
+++ print_stack.py
@@ -4,22 +4,36 @@
 from sys import exit, stderr
 
 
-def pall(stack):
-    """
-    prints all elements of a stack
-    """
-    length = len(stack)
-    for i in range(length - 1, -1, -1):
-        print(stack[i])
-
-
 def pint(line_number, stack):
     """
     prints the top element of a stack
     """
-    length = len(stack)
-    if length == 0:
+    if len(stack) == 0:
         print("L{:d}: can't pint, stack empty".format(
                     line_number), file=stderr)
         exit(1)
-    print(stack[length - 1])
+    print(stack[-1])
+
+
+def pall(line_number, stack):
+    """
+    prints all elements of a stack
+    """
+    length = len(stack)
+    if length > 0:
+        for i in range(length):
+            pint(line_number, stack[0:i + 1])
+
+
+def pchar(line_number, stack):
+    """
+    prints the character of the integer value at top of stack
+    """
+    return
+
+    
+def pstr(stack):
+    """
+    prints the characters of the integer values in a stack
+    """
+    return
