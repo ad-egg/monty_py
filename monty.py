2b1f255ab73c98b7fbb37f674a47352e8c69f584
Athena <539@holbertonschool.com>
2019-08-12 01:04:44 +0000

    changed dictionary value

--- monty.py
+++ monty.py
@@ -6,38 +6,45 @@ import os
 from manipulate_stack import *
 from print_stack import *
 
+from collections import deque
 from sys import argv, exit, stderr
 
 
 def line_reader(line_number, line, stack):
     """
-    takes a string, splits it, executes any commands on a stack
+    takes a string, splits it, executes any commands on a stack or queue
     """
     skip = {
         "nop", "#",
     }
+    t = {
+        "stack", "queue",
+    }
     printing = {
-        "pall": "pall(line_number, stack)",
-        "pint": "pint(line_number, stack)",
-        "pstr": "pstr(line_number, stack)",
-        "pchar": "pchar(line_number, stack)",
+        "pall": "pall(line_number, structure)",
+        "pint": "pint(line_number, structure)",
+        "pstr": "pstr(line_number, structure)",
+        "pchar": "pchar(line_number, structure)",
     }
     manipulating = {
-        "add": "add(line_number, stack)",
-        "sub": "sub(line_number, stack)",
-        "mul": "mul(line_number, stack)",
-        "div": "div(line_number, stack)",
-        "mod": "mod(line_number, stack)",
-        "push": "push(line_number, stack, line)",
-        "pop": "pop(line_number, stack)",
-        "rotl": "rotl(line_number, stack)",
-        "rotr": "rotr(line_number, stack)",
-        "swap": "swap(line_number, stack)",
+        "add": "add(line_number, structure)",
+        "sub": "sub(line_number, structure)",
+        "mul": "mul(line_number, structure)",
+        "div": "div(line_number, structure)",
+        "mod": "mod(line_number, structure)",
+        "push": "push(line_number, structure, line, s_q)",
+        "pop": "pop(line_number, structure)",
+        "rotl": "rotl(line_number, structure)",
+        "rotr": "rotr(line_number, structure)",
+        "swap": "swap(line_number, structure)",
     }
+    s_q = "stack"
     words = line.split()
     try:
         opcode = words[0]
-        if opcode in skip:
+        if opcode in t:
+            s_q = opcode
+        elif opcode in skip:
             return
         elif opcode in printing.keys():
             eval(printing[opcode])
@@ -54,7 +61,7 @@ def line_reader(line_number, line, stack):
 def monty(argv):
     """
     opens a Monty bytecodes file, reads it, and
-    executes the commands on a stack data structure
+    executes the commands on a stack/queue data structure
 
     """
     num_args = len(argv)
@@ -74,11 +81,11 @@ def monty(argv):
         lines = f.read()
     lines = lines.split("\n")
     line_number = 0
-    stack = []
+    structure = deque([])
     for line in lines:
         # send entire line to helper function
-        # helper function splits line and manipulates stack
-        line_reader(line_number, line, stack)
+        # helper function splits line and manipulates stack/queue
+        line_reader(line_number, line, structure)
         line_number += 1
     return
 
